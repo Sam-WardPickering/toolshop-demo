20334035aef0f72d263e95aa778f990b8824fcbe
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-04-14 18:03:43 +1200

    Create registerUser method

--- tests/pages/PageRegister.js
+++ tests/pages/PageRegister.js
@@ -14,4 +14,20 @@ export class PageRegister {
         this.passwordInput = page.getByTestId('password');
         this.registerButton = page.getByTestId('register-submit');
     }
+
+    async registerUser(userObj, userEmail) {
+        await this.firstNameInput.fill(userObj.firstName);
+        await this.firstNameInput.fill(userObj.lastName);
+        await this.dobInput.fill(userObj.dob);
+        await this.streetInput.fill(userObj.street);
+        await this.postalCode.fill(userObj.postalCode);
+        await this.cityInput.fill(userObj.city);
+        await this.stateInput.fill(userObj.state);
+        await this.countryInput.fill(userObj.country);
+        await this.phoneNumberInput.fill(userObj.phoneNumber);
+        await this.passwordInput.fill(userObj.password);
+        await this.emailInput.fill(userEmail);
+
+        await this.registerButton.click();
+    }
 }

874876699078c062bea9121f6da881f4d063833e
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-04-14 17:12:52 +1200

    Create folder structure && set up initial happy path test for registration

---
+++ tests/pages/auth/register.spec.js
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Registration', () => {
+    test('new customer registration (happy path)', async ({ page }) => {
+        await page.goto('/auth/register');
+
+        await page.pause();
+    }); 
+});

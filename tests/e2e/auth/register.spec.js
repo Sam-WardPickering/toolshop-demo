e20caff38ff8d07bbdcb9fec5ba5f5278b810c53
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-04-15 21:32:28 +1200

    Create < 18 age validation test

--- tests/e2e/auth/register.spec.js
+++ tests/e2e/auth/register.spec.js
@@ -16,8 +16,17 @@ test.describe('Registration', () => {
 
         await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible();
     }); 
-    test('customer registration rejected when age value < 18', async ({ page }) => {
-        
+    test.only('customer registration rejected when age value < 18', async ({ page }) => {
+        const email = generateValidEmail();
+        const userObj = { ...users.sam, email, dob: '2015-06-23' } // TODO - dynamic date generation
+
+        await page.goto('/auth/register');
+
+        const pageRegister = new PageRegister(page);
+
+        await pageRegister.registerUser(userObj);
+
+        await page.pause();
     });
     test('customer registration accepted when age value = 18', async ({ page }) => {
         

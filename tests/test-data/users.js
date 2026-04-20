851050e26fd1c249b5b40d9248094eeb61bc4929
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-04-20 16:26:24 +1200

    Add more default user data

--- tests/test-data/users.js
+++ tests/test-data/users.js
@@ -1,5 +1,6 @@
 export const users = {
     sam: {
+       role: 'user',
        firstName: 'Sam',
        lastName: 'Ward-Pickering',
        dob: '1997-06-23',
@@ -11,5 +12,20 @@ export const users = {
        phoneNumber: '0226350956',
        email: 'samuel.wardpickering@gmail.com',
        password: 'Password123456789!@'
+    },
+    admin: {
+        role: 'admin',
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'admin@practicesoftwaretesting.com',
+        password: 'welcome01',
+    },
+    defaultUser: {
+        role: 'user',
+        firstName: 'Jane',
+        lastName: 'Doe',
+        email: 'customer@practicesoftwaretesting.com',
+        password: 'welcome01',
     }
+
 }

a09f7e38af48bca0bb3fac5857d612fd63ca22fb
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-04-29 17:35:05 +1200

    Add first login test

--- tests/e2e/auth/login.spec.js
+++ tests/e2e/auth/login.spec.js
@@ -1,2 +1,10 @@
 import { test, expect } from '@playwright/test';
 import { PageLogin } from '../../pages/index.js';
+
+test.describe('Login', () => {
+    test('Login is successful (happy path)', async ({ page }) => {
+        await page.goto('/auth/login');
+
+        
+    });
+})

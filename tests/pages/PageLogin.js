7066b5aad1f13d6e6f2c4b8294294afc1caf476f
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-04-29 17:27:42 +1200

    Add page elements to pagelogin

--- tests/pages/PageLogin.js
+++ tests/pages/PageLogin.js
@@ -1,6 +1,9 @@
 export class PageLogin {
     constructor(page) {
         this.page = page;
-        
+
+        this.emailInput = page.getElementById('email');
+        this.passwordInput = page.getElementById('password');
+        this.loginButton = page.getElementById('login-submit');
     }
 } 

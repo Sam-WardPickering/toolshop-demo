f18afa986d740f859de1dcc200610cb5c04e4482
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-04-29 17:43:52 +1200

    Complete first login happy path test

--- tests/e2e/auth/login.spec.js
+++ tests/e2e/auth/login.spec.js
@@ -1,10 +1,22 @@
 import { test, expect } from '@playwright/test';
 import { PageLogin } from '../../pages/index.js';
+import { users } from '../../test-data/users.js';
 
 test.describe('Login', () => {
-    test('Login is successful (happy path)', async ({ page }) => {
+    test.only('Login is successful (happy path)', async ({ page }) => {
         await page.goto('/auth/login');
 
-        
+        const pageLogin = new PageLogin(page);
+
+        const userObj = { 
+            email: users.admin.email, 
+            password: users.admin.password
+        };
+
+        await pageLogin.login(userObj);
+
+        await expect(page.getByTestId('nav-menu')).toContainText('John Doe');
+        await expect(page).toHaveURL(/dashboard/);
+
     });
 })

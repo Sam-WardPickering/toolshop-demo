78753b44929ce3754fbaf91bd7bc1b33aea4e358
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-05-01 16:04:52 +1200

    Add page elements to constructor

--- tests/pages/PageForgotPassword.js
+++ tests/pages/PageForgotPassword.js
@@ -1,6 +1,9 @@
 export class PageForgotPassword {
     constructor(page) {
         this.page = page;
+        this.emailInput = page.getByTestId('email');
+        this.setNewPasswordButton = page.getByTestId('forgot-password-submit');
+        
         
     }
 }

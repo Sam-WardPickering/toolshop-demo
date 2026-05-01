0cd18b2ea097d7047dd1ccb998a223d28f407bf2
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-05-01 16:19:44 +1200

    Add PageForgotPassword to barrel file and import in forgotPassword.spec

--- tests/e2e/auth/forgotPassword.spec.js
+++ tests/e2e/auth/forgotPassword.spec.js
@@ -1,2 +1,14 @@
 import { test, expect } from '@playwright/test'
-import { PageLogin } from '../../pages/index.js';
+import { PageLogin, PageForgotPassword } from '../../pages/index.js';
+import { users } from '../../test-data/users.js';
+
+test.describe('Forgot Password', () => {
+    test('User can login successfuly after pasword reset', async ({ page }) => {
+        await page.goto('/auth/register');
+
+        const pageLogin = new PageLogin(page);
+        const pageForgotPassword = new PageForgotPassword(page);
+
+
+    });
+});
--- tests/pages/index.js
+++ tests/pages/index.js
@@ -1,2 +1,3 @@
 export { PageRegister } from './PageRegister.js';
 export { PageLogin } from './PageLogin.js';
+export { PageForgotPassword } from './PageForgotPassword.js';

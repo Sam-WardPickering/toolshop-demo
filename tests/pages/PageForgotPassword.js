4d7c7b34fccbce89f35d7e4b5c768a8cd9b6170e
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-05-01 16:15:23 +1200

    Add submit forgot password methid

--- tests/pages/PageForgotPassword.js
+++ tests/pages/PageForgotPassword.js
@@ -3,7 +3,10 @@ export class PageForgotPassword {
         this.page = page;
         this.emailInput = page.getByTestId('email');
         this.setNewPasswordButton = page.getByTestId('forgot-password-submit');
-        
-        
+    }
+
+    async submitForgotPassword(email) {
+        this.emailInput.fill(email);
+        this.setNewPasswordButton.click();
     }
 }

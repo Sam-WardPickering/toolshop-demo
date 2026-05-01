e1eb3333bdde6ceedd8564c636cf9f9d21cc2a9b
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-05-01 21:45:22 +1200

    Add logic to first test for logging in succcessfuly after password reset

--- tests/e2e/auth/forgotPassword.spec.js
+++ tests/e2e/auth/forgotPassword.spec.js
@@ -3,11 +3,26 @@ import { PageLogin, PageForgotPassword } from '../../pages/index.js';
 import { users } from '../../test-data/users.js';
 
 test.describe('Forgot Password', () => {
-    test('User can login successfuly after pasword reset', async ({ page }) => {
-        await page.goto('/auth/register');
+    test.only('User can login successfuly after pasword reset (happy path)', async ({ page }) => {
+        await page.goto('/auth/forgot-password');
+
+        const userObj = {
+            ...users.defaultUser,
+            password: 'welcome02'
+        }
+
+        const pageForgotPassword = new PageForgotPassword(page);
+
+        await pageForgotPassword.submitForgotPassword(users.defaultUser.email);
+
+        await expect(page.getByRole('alert')).toContainText('page.forgot-password.confirm');
+
+        await page.goto('/auth/login');
 
         const pageLogin = new PageLogin(page);
-        const pageForgotPassword = new PageForgotPassword(page);
+
+        await pageLogin.login(userObj);
+
 
 
     });

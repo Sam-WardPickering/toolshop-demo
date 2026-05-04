0252dff9113af0e2afb05e8d93685bfaf38791f8
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-05-04 17:19:44 +1200

    Update fixture to use default user instead of admin

--- tests/fixtures/baseTest.js
+++ tests/fixtures/baseTest.js
@@ -8,13 +8,14 @@ export const test = base.extend({
         const pageLogin = new PageLogin(page);
 
         const userObj = { 
-            email: users.admin.email, 
-            password: users.admin.password
+            email: users.defaultUser.email, 
+            password: users.defaultUser.password
         };
 
         await pageLogin.login(userObj);
 
-        await expect(page.getByTestId('nav-menu')).toContainText('John Doe');
+        await expect(page.getByTestId('nav-menu')).toContainText('Jane Doe');
         await expect(page).toHaveURL(/dashboard/);
+        await use({ page });
     },
 });

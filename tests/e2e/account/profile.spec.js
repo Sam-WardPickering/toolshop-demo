1a4d80b5a54f0cb8772680bfb6fb5e9062be0ef3
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-05-06 20:04:44 +1200

    Refactor

--- tests/e2e/account/profile.spec.js
+++ tests/e2e/account/profile.spec.js
@@ -2,7 +2,7 @@ import { PageProfile } from '../../pages/index.js';
 import { test, expect } from '../../fixtures/baseTest.js';
 
 test.describe('Profile', () => {
-    test.only('', async ({ loggedInPage: { page } }) => {
+    test('', async ({ loggedInPage: { page } }) => {
         // tests
         await page.goto('/account/profile');
 

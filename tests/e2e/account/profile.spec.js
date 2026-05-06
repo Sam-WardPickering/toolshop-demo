c3cf414223e024afe9edd0ec20cf4aac2864079e
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-05-06 22:37:13 +1200

    Add beforeEach to handle navigation to profile page

--- tests/e2e/account/profile.spec.js
+++ tests/e2e/account/profile.spec.js
@@ -2,10 +2,13 @@ import { PageProfile } from '../../pages/index.js';
 import { test, expect } from '../../fixtures/baseTest.js';
 
 test.describe('Profile', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('/account/profile');
+    });
     test('', async ({ loggedInPage: { page } }) => {
         // tests
         await page.goto('/account/profile');
 
-        await page.pause();
+       
     })
 })

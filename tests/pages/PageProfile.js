8c8685ce0db8e31878f5d407e76a70ecef987dc4
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-05-06 22:41:35 +1200

    Add change password inputs and buttons to PageProfile

--- tests/pages/PageProfile.js
+++ tests/pages/PageProfile.js
@@ -2,6 +2,7 @@ export class PageProfile {
     constructor(page) {
         this.page = page;
 
+        /* Profile Detail Section */
         firstNameInput = page.getByTestId('first-name');
         lastNameInput = page.getByTestId('last-name');
         emailInput = page.getByTestId('email');
@@ -11,8 +12,13 @@ export class PageProfile {
         cityInput = page.getByTestId('city');
         stateInput = page.getByTestId('state');
         countryInput = page.getByTestId('country');
-
         updateProfileBtn = page.getByTestId('update-profile-submit');
-        
+
+        /* Change password section */
+        currentPasswordInput = page.getByTestId('current-password');
+        newPasswordInput = page.getByTestId('new-password');
+        confirmNewPasswordInput = page.getByTestId('new-password-confirm');
+        changePasswordBtn = page.getByTestId('change-password-submit');
+
     }
 }

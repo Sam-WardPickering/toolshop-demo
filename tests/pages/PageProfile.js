8ae50bb718948275abd5c04ac4c72704aa408791
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-05-06 19:54:49 +1200

    Add buttons and inputs for pageprofile

--- tests/pages/PageProfile.js
+++ tests/pages/PageProfile.js
@@ -1,5 +1,18 @@
 export class PageProfile {
     constructor(page) {
         this.page = page;
+
+        firstNameInput = page.getByTestId('first-name');
+        lastNameInput = page.getByTestId('last-name');
+        emailInput = page.getByTestId('email');
+        phoneInput = page.getByTestId('phone');
+        streetInput = page.getByTestId('street');
+        postCodeInput = page.getByTestId('postal_code');
+        cityInput = page.getByTestId('city');
+        stateInput = page.getByTestId('state');
+        countryInput = page.getByTestId('country');
+
+        updateProfileBtn = page.getByTestId('update-profile-submit');
+        
     }
 }

271f3858bc36860a994d32ebc63b6305fb6015c0
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-05-06 19:49:00 +1200

    Create PageProfile and export it from barrel file

--- tests/pages/index.js
+++ tests/pages/index.js
@@ -1,3 +1,4 @@
 export { PageRegister } from './PageRegister.js';
 export { PageLogin } from './PageLogin.js';
 export { PageForgotPassword } from './PageForgotPassword.js';
+export { PageProfile } from './PageProfile.js'

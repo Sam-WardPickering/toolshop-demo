8ab649551a277e236bf28909aaa0375aad99d74c
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-05-18 18:41:41 +1200

    Create init get test

--- tests/api/brand.spec.js
+++ tests/api/brand.spec.js
@@ -1,1 +1,8 @@
 import { test, expect } from '@playwright/test';
+
+test('GET all brands', async ({ request }) => {
+    const response = await request.get('/brands');
+    const responseJson = await response.json();
+
+    await expect(response.status()).toBe(200);
+});

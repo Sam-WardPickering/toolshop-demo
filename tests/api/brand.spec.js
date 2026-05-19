aca111443b14162cac2c43b2bd973625121d9d86
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-05-19 13:13:48 +1200

    Add check for response properties:

--- tests/api/brand.spec.js
+++ tests/api/brand.spec.js
@@ -9,4 +9,10 @@ test('GET all brands', async ({ request }) => {
 
     await expect(Array.isArray(responseJson)).toBeTruthy();
     await expect(responseJson.length).toBeGreaterThan(0);
+
+    for (const brand of responseJson) {
+        expect(brand).toHaveProperty('id');
+        expect(brand).toHaveProperty('name');
+        expect(brand).toHaveProperty('slug');
+    }
 });

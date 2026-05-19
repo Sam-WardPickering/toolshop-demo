e791ae7864ec4e442dcdfb697dc5c18c6c55e2a0
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-05-19 13:19:00 +1200

    Add tests in describe block

--- tests/api/brand.spec.js
+++ tests/api/brand.spec.js
@@ -1,17 +1,23 @@
 import { test, expect } from '@playwright/test';
 
-test('GET all brands', async ({ request }) => {
-    const response = await request.get('/brands');
-    const responseJson = await response.json();
+test.describe('GET requests', () => {
+    test('GET all brands', async ({ request }) => {
+        const response = await request.get('/brands');
+        const responseJson = await response.json();
 
-    await expect(response.status()).toBe(200);
+        expect(response.status()).toBe(200);
 
-    await expect(Array.isArray(responseJson)).toBeTruthy();
-    await expect(responseJson.length).toBeGreaterThan(0);
+        await expect(Array.isArray(responseJson)).toBeTruthy();
+        await expect(responseJson.length).toBeGreaterThan(0);
 
-    for (const brand of responseJson) {
-        expect(brand).toHaveProperty('id');
-        expect(brand).toHaveProperty('name');
-        expect(brand).toHaveProperty('slug');
-    }
+        for (const brand of responseJson) {
+            expect(brand).toHaveProperty('id');
+            expect(brand).toHaveProperty('name');
+            expect(brand).toHaveProperty('slug');
+        }
+    });
+    
 });
+
+
+

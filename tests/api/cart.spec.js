82e0783357247f8d686e156e7623e740ad68ec5e
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-05-25 18:38:28 +1200

    Refactor first test

--- tests/api/cart.spec.js
+++ tests/api/cart.spec.js
@@ -2,11 +2,11 @@ import { test, expect } from '@playwright/test';
 import { randomUUID } from 'node:crypto';
 
 test.describe('POST', () => {
-    test.only('Create a new cart (happy path)', async ({ request }) => {
+    test('Create a new cart (happy path)', async ({ request }) => {
         const newCartResponse = await request.post('/carts');
 
         expect(newCartResponse.status()).toBe(201);
 
-        expect((await newCartResponse.json()).id).toBeTruthy();
+        expect((await newCartResponse.json()).id).toBeDefined();
     });
 });

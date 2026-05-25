72647f0ea2bf04c83c1a855956dc36d4f9b1851b
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-05-25 18:36:45 +1200

    Add happy path create cart test

--- tests/api/cart.js
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-import { randomUUID } from 'node:crypto';
-
-test.describe('POST', () => {
-    test('Create a new cart (happy path)', async ({ request }) => {
-
-    });
-});

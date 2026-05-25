07a2969bd58a48b4694f59796c0b40633746ee08
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-05-25 18:30:29 +1200

    Add init describe block and test

--- tests/api/cart.js
+++ tests/api/cart.js
@@ -1,1 +1,8 @@
 import { test, expect } from '@playwright/test';
+import { randomUUID } from 'node:crypto';
+
+test.describe('POST', () => {
+    test('Create a nbew cart (happy path)', async ({ request }) => {
+        
+    });
+});

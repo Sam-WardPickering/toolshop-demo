676fe647138442dcbe38fa8e41657be4d4fb1ff5
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-05-26 18:34:25 +1200

    Create PUT test

--- tests/api/cart.spec.js
+++ tests/api/cart.spec.js
@@ -67,11 +67,58 @@ test.describe('POST', () => {
             expect(getCartJson.id).toBe(cartId);
         });
     });
-    
+
 
     test.describe('PUT', () => {
         test('Update quantity of cart item', async ({ request }) => {
+            /* Get existing product for id */
+            const products = await request.get('/products');
+            expect(products.status()).toBe(200);
 
+            const productsJson = await products.json();
+
+            /* Get ID from first product returned */
+            const productId = productsJson.data[0].id;
+
+            /* Create new cart */
+            const newCartResponse = await request.post('/carts');
+
+            expect(newCartResponse.status()).toBe(201);
+
+            /* Get cart ID & add cart item*/
+            const newCartId = await (await newCartResponse.json()).id;
+
+            expect(newCartId).toBeDefined();
+
+            const cartItem = {
+                'product_id': productId, 
+                'quantity': 1
+            };
+
+            const cartItemResponse = await request.post(`/carts/${newCartId}`, {
+                data: cartItem,
+            });
+
+            expect(cartItemResponse.status()).toBe(200);
+
+            expect((await cartItemResponse.json()).result).toBe('item added or updated');
+
+            /* Update quantity of cart item */
+
+            const updatedQuantity = {
+                ...cartItem,
+                'quantity': 67
+            };
+
+            const updateQuantity = await request.put(`/carts/${newCartId}/product/quantity`, {
+                data: updatedQuantity,
+            });
+
+            expect(updateQuantity.status()).toBe(200);
+
+            updateQuantityJson = await updateQuantity.json();
+
+            expect(updateQuantityJson.success).toBe(true);
         });
     });
 
